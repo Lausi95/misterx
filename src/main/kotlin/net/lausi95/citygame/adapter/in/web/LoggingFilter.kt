617a5a2f349326008206f84117c73d17298cffdf
Tom Lausmann <tomlausmann@gmail.com>
2026-06-22 13:05:41 +0200

    feat: reenable user resolving

--- src/main/kotlin/net/lausi95/citygame/adapter/in/web/LoggingFilter.kt
+++ src/main/kotlin/net/lausi95/citygame/adapter/in/web/LoggingFilter.kt
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.jboss.logging.MDC
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 import java.util.*
@@ -16,7 +17,7 @@ class LoggingFilter : OncePerRequestFilter() {
         filterChain: FilterChain
     ) {
         val traceId = UUID.randomUUID()
-        val user = "none" // SecurityContextHolder.getContext().authentication?.name
+        val user = SecurityContextHolder.getContext().authentication?.name ?: "none"
         val path = request.servletPath
 
         MDC.put("trace_id", traceId)
